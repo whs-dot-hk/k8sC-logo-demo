90b9e782518d3e4991208477a345d2a810f1b9ea
Wong Heung Sang <hswongac@gmail.com>
2018-06-24 23:22:32 +0800

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pathPrefix: "/k8sC-logo-demo",
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/components/header.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import logo from './logo.svg';
+import logoInactive from './logo-gray.svg';
+import community from './community.svg';
+import communityShort from './community-short.svg';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  state = {
+    logoActivated: true,
+    logoSmall: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({logoSmall: window.innerWidth <= 960});
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          //background: 'rebeccapurple',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
+          className="w-90 pv1 pv3-l"
+          style={{
+            margin: '0 auto',
+            //maxWidth: 960,
+            //padding: '1.45rem 1.0875rem',
+          }}
+        >
+          <a
+            className="di"
+          >
+            <img
+              className="mb0 mr2"
+              src={this.state.logoActivated ? logo : logoInactive}
+              height= {!this.state.logoSmall ? "30" : "20"}
+              alt="Logo"
+            />
+            <img
+              className="mb0 dn dib-l"
+              src={community}
+              height="30"
+              alt="Community"
+            />
+            <img
+              className="mb0 dib dn-l"
+              src={communityShort}
+              height= {!this.state.logoSmall ? "30" : "20"}
+              alt="Community"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header
